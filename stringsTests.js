63459d4b7ad7588fa9461a44ffcd47f672710202
Diego Pacheco <diegopacheco.baca@gmail.com>
2022-07-18 14:58:12 -0500

    al dia

---
+++ stringsTests.js
@@ -0,0 +1,6 @@
+let str_x = "   Hola soy un string con espacios al final.     "
+
+console.log(str_x.length)
+console.log(str_x.trim().length)
+console.log(str_x.trimStart().length)
+console.log(str_x.trimEnd().length)
